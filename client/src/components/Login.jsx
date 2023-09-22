c0e2c09816f9493599383d4d6daaf84e4dff582a
utkarsh1311 <itsutkarsh123@gmail.com>
2023-09-22 22:34:46 +0530

    add login page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import loginImg from "../assets/login.jpg";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+    const navigate = useNavigate();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const onSubmit = (data) => {
+		console.log(data);
+		localStorage.setItem("user", JSON.stringify(data));
+		if (data.email == "admin@gmail.com" && data.password == "admin123") {
+			navigate("/admin");
+		} else if (
+			data.email == "teacher@gmail.com" &&
+			data.password == "teacher123"
+		) {
+			navigate("/teacher");
+		} else {
+			return <Login />;
+		}
+	};
+	return (
+		<div className="w-screen h-screen bg-gray-100 flex justify-center items-center px-10 text-gray-600">
+			<div className=" bg-gray-300 flex rounded-md border-2  border-gray-200 overflow-hidden">
+				<div className="w-1/2 object-fill md:hidden">
+					<img className="" src={loginImg} alt="login form image" />
+				</div>
+				<div className="flex-grow px-8 py-10  justify-center flex flex-col">
+					<h2 className="text-5xl bold uppercase mb-6 font-extrabold ">
+						Login{" "}
+					</h2>
+					<form
+						action=""
+						className="flex flex-col gap-4"
+						onSubmit={handleSubmit(onSubmit)}>
+						<div className="flex flex-col w-full">
+							<label htmlFor="email">Email</label>
+							<input
+								placeholder="example@mail.com"
+								{...register("email", {
+									required: "Email is required.",
+									pattern: {
+										value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+										message: "Email is not valid.",
+									},
+								})}
+								type="text"
+								name="email"
+								id="email"
+								className="h-10 px-4 py-2 outline-none border-none rounded-md outline-1 font-semibold outline-gray-300 "
+							/>
+							{errors.email && (
+								<p className="text-sm font-semibold text-red-600 mt-1">
+									{errors.email.message}
+								</p>
+							)}
+						</div>
+						<div className="flex flex-col">
+							<label htmlFor="password">Password</label>
+							<input
+								{...register("password", {
+									required: "Password is required",
+									minLength: { value: 8, message: "Too small" },
+								})}
+								type="password"
+								name="password"
+								placeholder="********"
+								id="password"
+								className="h-10 px-4 py-2 outline-none border-none rounded-md outline-1 font-semibold outline-gray-300 "
+							/>
+							{errors.password && (
+								<p className="text-sm font-semibold text-red-600 mt-1">
+									{errors.password.message}
+								</p>
+							)}
+						</div>
+						<div className="">
+							<button
+								className="w-full rounded-md bg-blue-400 hover:bg-blue-500 py-2 font-semibold text-lg text-white duration-150 ease-in transition-colors"
+								type="submit">
+								Login
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+      fontFamily: {
+        poppins : ['Poppins', 'sans-serif']
+      }
+    },
+		screens: {
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
+	},
+	plugins: [],
+};
