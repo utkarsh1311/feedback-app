6cc962b9e28cea8662423d775946cf5fbc27e9cd
utkarsh maurya <itsutkarsh123@gmail.com>
2023-12-25 01:11:15 +0530

    refactor: refactor service files and update API endpoints

--- src/services/feedbackService.js
+++ src/services/feedbackService.js
@@ -2,43 +2,29 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
-const teacherToken = import.meta.env.VITE_TEACHER_TOKEN;
-const adminToken = import.meta.env.VITE_ADMIN_TOKEN;
 
-const config = {
-	headers: {
-		Authorization: `Bearer ${teacherToken}`,
-	},
-};
-
-const configForAdmin = {
-	headers: {
-		Authorization: `Bearer ${adminToken}`,
-	},
-};
-
-const getAllFeedbacks = async () => {
-	const res = await axios.get(`${BASE_URL}/feedbacks`, configForAdmin);
+const getAllFeedbacks = async config => {
+	const res = await axios.get(`${BASE_URL}/feedbacks`, config);
 	return res.data;
 };
 
-const getFeedbackById = async id => {
+const getFeedbackById = async (id,config)=> {
 	const res = await axios.get(`${BASE_URL}/feedbacks/${id}`, config);
 	return res.data;
 };
 
-const createFeedback = async feedback => {
+const createFeedback = async (feedback, config) => {
 	const res = await axios.post(`${BASE_URL}/feedbacks`, feedback, config);
 	return res.data;
 };
 
-const updateFeedback = async (id, feedback) => {
-	const res = await axios.put(`${BASE_URL}/feedbacks/${id}`, feedback, configForAdmin);
+const updateFeedback = async (id, feedback, config) => {
+	const res = await axios.put(`${BASE_URL}/feedbacks/${id}`, feedback, config);
 	return res.data;
 };
 
-const deleteFeedback = async id => {
-	const res = await axios.delete(`${BASE_URL}/feedbacks/${id}`, configForAdmin);
+const deleteFeedback = async (id, config) => {
+	const res = await axios.delete(`${BASE_URL}/feedbacks/${id}`, config);
 	return res.data;
 };
 
--- src/services/teacherService.js
+++ src/services/teacherService.js
@@ -1,41 +1,33 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL;
-console.log(BASE_URL);
-const adminToken = import.meta.env.VITE_ADMIN_TOKEN;
-
-const config = {
-	headers: {
-		Authorization: `Bearer ${adminToken}`,
-	},
-};
 
 const teacherLogin = async teacher => {
-	const res = await axios.post(`${BASE_URL}/teacher/login`, teacher);
+	const res = await axios.post(`${BASE_URL}/teachers/login`, teacher);
 	return res.data;
 };
 
-const getAllTeachers = async () => {
+const getAllTeachers = async config => {
 	const res = await axios.get(`${BASE_URL}/teachers`, config);
 	return res.data;
 };
 
-const getTeacherById = async id => {
+const getTeacherById = async (id, config) => {
 	const res = await axios.get(`${BASE_URL}/teachers/${id}`, config);
 	return res.data;
 };
 
-const createTeacher = async teacher => {
+const createTeacher = async (teacher, config) => {
 	const res = await axios.post(`${BASE_URL}/teachers`, teacher, config);
 	return res.data;
 };
 
-const updateTeacher = async (id, teacher) => {
+const updateTeacher = async (id, teacher, config) => {
 	const res = await axios.put(`${BASE_URL}/teachers/${id}`, teacher, config);
 	return res.data;
 };
 
-const deleteTeacher = async id => {
+const deleteTeacher = async (id, config) => {
 	const res = await axios.delete(`${BASE_URL}/teachers/${id}`, {
 		...config,
 	});
