ee5c7c4c8db6c4370db14ac208d6948684242e2e
utkarsh maurya <itsutkarsh123@gmail.com>
2023-12-25 01:10:32 +0530

    feat: add login functionality and UI improvements

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,75 @@
-import loginImg from "../assets/login.jpg";
+import { useLayoutEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import loginImg from "../assets/login.jpg";
+import showpass from "../assets/showpass.png";
+import adminService from "../services/adminService";
+import teacherService from "../services/teacherService";
 
 const Login = () => {
+	const [loader, setLoader] = useState(false);
+	const [loginAs, setLoginAs] = useState("teacher");
+	const [showpassword, setShowpassword] = useState(false);
+
 	const navigate = useNavigate();
+
 	const {
 		register,
 		handleSubmit,
 		formState: { errors },
 	} = useForm();
 
-	const onSubmit = data => {
+	useLayoutEffect(() => {
+		const token = JSON.parse(localStorage.getItem("token"));
+
+		if (token) {
+			if (token.role === "ADMIN") {
+				navigate("/admin");
+			} else if (token.role === "TEACHER") {
+				navigate("/teacher");
+			}
+		}
+	}, []);
+
+	const onSubmit = async data => {
 		console.log(data);
-		localStorage.setItem("user", JSON.stringify(data));
-		if (
-			data.email == import.meta.env.VITE_ADMIN_MAIL &&
-			data.password == import.meta.env.VITE_ADMIN_PASSWORD
-		) {
-			navigate("/admin");
-		} else if (
-			data.email == import.meta.env.VITE_TEACHER_MAIL &&
-			data.password == import.meta.env.VITE_TEACHER_PASSWORD
-		) {
-			navigate("/teacher");
-		} else {
-			return <Login />;
+
+		try {
+			setLoader(true);
+			if (loginAs === "admin") {
+				const res = await adminService.adminLogin(data);
+				console.log(res);
+				const userData = {
+					id: res.data.id,
+					token: res.data.token,
+					role: res.data.role,
+				};
+				localStorage.setItem("token", JSON.stringify(userData));
+				setLoader(false);
+				alert("Login Successful");
+				navigate("/admin");
+				return;
+			} else if (loginAs === "teacher") {
+				const res = await teacherService.teacherLogin(data);
+				console.log(res);
+				const userData = {
+					id: res.data.id,
+					token: res.data.token,
+					role: res.data.role,
+				};
+				localStorage.setItem("token", JSON.stringify(userData));
+				setLoader(false);
+				alert("Login Successful");
+				navigate("/teacher");
+				return;
+			}
+		} catch (error) {
+			setLoader(false);
+			// alert(error.response.data.message);
+			console.log(error);
 		}
 	};
+
 	return (
 		<div className="w-screen h-screen bg-gray-100 flex justify-center items-center px-10 text-gray-600">
 			<div className=" bg-gray-300 flex rounded-md border-2  border-gray-200 overflow-hidden">
@@ -66,17 +109,25 @@ const Login = () => {
 						</div>
 						<div className="flex flex-col">
 							<label htmlFor="password">Password</label>
-							<input
-								{...register("password", {
-									required: "Password is required",
-									minLength: { value: 8, message: "Too small" },
-								})}
-								type="password"
-								name="password"
-								placeholder="********"
-								id="password"
-								className="h-10 px-4 py-2 outline-none border-none rounded-md outline-1 font-semibold outline-gray-300 "
-							/>
+							<div className="flex bg-white rounded-md outline-1 h-10 outline-gray-300 ">
+								<input
+									{...register("password", {
+										required: "Password is required",
+										minLength: { value: 8, message: "Too small" },
+									})}
+									type={showpassword ? "text" : "password"}
+									name="password"
+									placeholder="********"
+									id="password"
+									className=" px-4 py-2 outline-none border-none w-full rounded-md outline-1 font-semibold "
+								/>
+								<img
+									src={showpass}
+									alt=""
+									className="  scale-50 w-fit cursor-pointer"
+									onClick={() => setShowpassword(!showpassword)}
+								/>
+							</div>
 							{errors.password && (
 								<p className="text-sm font-semibold text-red-600 mt-1">
 									{errors.password.message}
@@ -84,9 +135,34 @@ const Login = () => {
 							)}
 						</div>
 						<div className="">
+							<input
+								type="radio"
+								id="teacher"
+								value={`WhatsApp: +91 9654388797`}
+								checked={loginAs === `teacher`}
+								onChange={() => setLoginAs("teacher")}
+								className="mr-2"
+							/>
+							<label htmlFor="teacher" className="mr-4">
+								Teacher
+							</label>
+							<input
+								type="radio"
+								id="admin"
+								value="For classroom only"
+								checked={loginAs === `admin`}
+								onChange={() => setLoginAs("admin")}
+								className="mr-2"
+							/>
+							<label htmlFor="admin">Admin</label>
+						</div>
+						<div className="">
 							<button
-								className="w-full rounded-md bg-blue-400 hover:bg-blue-500 py-2 font-semibold text-lg text-white duration-150 ease-in transition-colors"
+								className={`w-full rounded-md bg-blue-400 hover:bg-blue-500 py-2 font-semibold text-lg text-white duration-150 ease-in transition-colors ${
+									loader ? "cursor-not-allowed" : " cursor-pointer"
+								}`}
 								type="submit"
+								disabled={loader}
 							>
 								Login
 							</button>
